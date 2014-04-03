3eaacc84c4aede5c1483520f4f4087880d157507
Laibin Xu <ext-laibin.xu@nokia.com>
2014-04-03 17:03:19 +0800

    get the json second data

--- python/parseJson/parseJson.py
+++ python/parseJson/parseJson.py
@@ -4,19 +4,10 @@ import json
 #f = open('device.json')
 f = open('Att31D4.tmp.json')
 #print f.read()
-fjson = json.loads(f.read())
+fjson = json.load(f)
+fjson2 = json.loads(fjson[0]['status'])
+print fjson2['id']
 
-print fjson[0]['id']
 #fjson = json.dumps(f.read(), sort_keys = True,indent = 4, separators = (',', ':'))
 #print fjson
 #s = json.loads('{"name":"test", "type":{"name":"seq", "parameter":["1", "2"]}}')
-
-
-
-
-
-
-
-
-
-
