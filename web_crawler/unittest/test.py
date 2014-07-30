6a5094a76b712d7fefb400be88a2d6b8d72541b4
labinxu <flowinair@gmail.com>
2014-07-30 09:28:01 +0800

    webcrawler

---
+++ web_crawler/unittest/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def testreg():
+    s = '123name123name123name'
+    # name = 123, name = 123 , name = 123
+    import re
+    # s = 'Hello, mhmyqn@qq.com or mhmyqn@126.com is my email.'
+    # m = re.findall('\w+@\w+\.\w+', s)
+    m = re.findall()
+
+    print m
+    #  = re.match('\w+', s)
+    # key = m.group(0)
+    # print key
+
+testreg()
+    
